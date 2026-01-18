f8205bfa744db03ed2a0278b44a705f44d071e66
kokozovaanel <253991@astanait.edu.kz>
2026-01-18 22:19:45 +0500

    Update PostgresDB.java

--- src/PostgresDB.java
+++ src/PostgresDB.java
@@ -1,1 +1,27 @@
-//connect to db
+import repositories.IDB;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgresDB implements IDB {
+    public POstgresDB(String host, String dbName, String user, String password) {
+        try {
+            String className;
+            Class.forName( "org.postgresql.Driver");
+            String url = host + "/" + dbName;
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.ptintln("PostgreSQL connected");
+        } catch (Exception e) {
+            System.out.println("DB error: " + e.getMessage());}
+
+    @Override
+    public Connection getConnection() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}      
