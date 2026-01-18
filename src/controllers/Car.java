649efd9ddc3e9decd8dcafca399442facf7cb8f6
Ra-0001 <romikk561@gmail.com>
2026-01-18 22:26:13 +0500

    add parameterized constructor to Car class

--- src/controllers/Car.java
+++ src/controllers/Car.java
@@ -6,3 +6,11 @@ public class Car {
     private String model;
     private double price;
     private boolean rented;
+
+    public Car(int id, String brand, String model, double price, String status) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.rented = status.equalsIgnoreCase("RENTED");
+    }
