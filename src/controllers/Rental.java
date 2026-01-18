a9f8a9d3f957966f783a15366447b5c1d8947640
Ra-0001 <romikk561@gmail.com>
2026-01-18 22:51:25 +0500

    add initial Rental model with id and carId

--- src/controllers/Rental.java
+++ src/controllers/Rental.java
@@ -1,1 +1,14 @@
-//cоздайте конструктор с параметрами int id and int user id
+package controllers;
+
+public class Rental {
+    private int id;
+    private int carId;
+
+    public Rental(int id, int carId) {
+        this.id = id;
+        this.carId = carId;
+    }
+
+    public int getId() { return id; }
+    public int getCarId() { return carId; }
+}
