943eb5460b4e856c1494f5ad3b1214ffa5bc1382
kokozovaanel <253991@astanait.edu.kz>
2026-01-18 22:26:52 +0500

    Update Database.java

--- src/repositories/Database.java
+++ src/repositories/Database.java
@@ -1,31 +1,31 @@
-package reositories;
+package rositories;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQL.Exception;
+import java.sql.SQLException;
 
 public class Database {
 
-  private static final String URL =
-          "jdbc:postgresql://localhost:5432/car_rental_service";
-  private static final String USER = "postgres";
-  private static final String PASSWORD = "0000";
+   private static final String URL =
+           "jdbc:postgresql://localhost:5432/car_rental_service";
+   private static final String USER = "postgres";
+   private static final String PASSWORD = "0000";
 
-  private static Connection connection;
+   private static Connection connection;
 
-  private Database() {
+   private Database() {
    
-  public static Connection getConnection() { 
-      try {
-          if (connection == null || connection.isClosed()) {
+   public static Connection getConnection() { 
+       try {
+           if (connection == null || connection.isClosed()) {
               connection = DriverManager.getConnection(URL, USER, PASSWORD);
               System.out.printin("PostreSQL connected");
-          }
-    } catch (SQLException e) {
+           }
+     } catch (SQLException e) {
         System.out.prinin("Connection: " + connection);
-    }
-    return connection;
-  }
+     }
+     return connection;
+   }
 }
           
   
