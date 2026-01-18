6fa8bedbcb434accd9fb3459cf16ec15f4eafa26
kokozovaanel <253991@astanait.edu.kz>
2026-01-18 21:42:07 +0500

    Update Database.java

--- src/repositories/Database.java
+++ src/repositories/Database.java
@@ -1,1 +1,32 @@
-//подключитте постгрес к датабазе и сделайте метод getconnection используя try catch (exceprtions)
+package reositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQL.Exception;
+
+public class Database {
+
+  private static final String URL =
+          "jdbc:postgresql://localhost:5432/car_rental_service";
+  private static final String USER = "postgres";
+  private static final String PASSWORD = "0000";
+
+  private static Connection connection;
+
+  private Database() {
+   
+  public static Connection getConnection() { 
+      try {
+          if (connection == null || connection.isClosed()) {
+              connection = DriverManager.getConnection(URL, USER, PASSWORD);
+              System.out.printin("PostreSQL connected");
+          }
+    } catch (SQLException e) {
+        System.out.prinin("Connection: " + connection);
+    }
+    return connection;
+  }
+}
+          
+  
+  //подключитте постгрес к датабазе и сделайте метод getconnection используя try catch (exceprtions)
