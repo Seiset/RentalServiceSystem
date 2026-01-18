2e2f7346ce3af273db59e64523d3002ac6b85a27
kokozovaanel <253991@astanait.edu.kz>
2026-01-18 22:07:31 +0500

    Update IDB.java

--- src/repositories/IDB.java
+++ src/repositories/IDB.java
@@ -1,1 +1,14 @@
-//create methods for successfull connection to db
+package repositories;
+
+import java.SQL.Connection;
+
+public interface IDB {
+   static Connection getConnection() {
+      return null;
+   }
+
+   Connection getConnection();
+
+   void close();
+
+}
