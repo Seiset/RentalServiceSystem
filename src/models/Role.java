03640466c3a23bdcdcb53acf93bd4dd1c603ce4f
Ra-0001 <romikk561@gmail.com>
2026-02-01 21:46:12 +0500

    add method for getting role from String

--- src/models/Role.java
+++ src/models/Role.java
@@ -4,3 +4,14 @@ public enum Role {
     ADMIN,
     MANAGER,
     USER;
+
+     public static Role fromString(String roleStr) {
+        if (roleStr == null) return null;
+        try {
+            return Role.valueOf(roleStr.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null; // or throw exception - depending on your needs
+        }
+    }
+
+
