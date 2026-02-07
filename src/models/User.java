1f6ef1384a811400eef9cab4d3c09d2fb969fed6
kokozovaanel <253991@astanait.edu.kz>
2026-02-07 22:46:29 +0500

    Update User.java

--- src/models/User.java
+++ src/models/User.java
@@ -4,24 +4,17 @@ public class User {
   private int id;
   private String name;
   private Role role;
+  private String password;
 
-  public User(int id, String name, Role role) {
+  public User(int id, String name, Role role, String password) {
       this.id = id;
       this.name = name;
       this.role = role;
+      this.password = password;
   }
 
   public int getId() { return id; }
   public String getName() { return name; }
   public Role getRole() { return role; }
-
-  @Override
-  public String toString() {
-      return "User{" +
-              "id=" + id +
-              ", name='" + name + '\'' +
-              ", role=" + role.getDisplayName() +
-              '}';
-   }
+  public String getPassword() { return password; }
 }
-  
